eec2e6e91ab5e65f9eda2ae67805395c926e3314
Max Gordon <max@gforge.se>
2021-11-25 14:41:57 +0100

    Added newdata to predint

--- R/lqmm_S3_predint.R
+++ R/lqmm_S3_predint.R
@@ -1,6 +1,15 @@
 
 #' @rdname lqmm_predictions
-predint.lqmm <- function(object, level = 0, alpha = 0.05, R = 50, seed = round(runif(1, 1, 10000)), newdata) {
+#' @param alpha 1-alpha is the confidence level.
+#' @param R	    number of bootstrap replications.
+#' @param seed  optional random number generator seed.
+predint.lqmm <- function(object,
+                         level = 0,
+                         alpha = 0.05,
+                         R = 50,
+                         seed = round(runif(1, 1, 10000)),
+                         newdata)
+{
   tau <- object$tau
   nq <- length(object$tau)
   p <- object$dim_theta[1]
@@ -25,7 +34,10 @@ predint.lqmm <- function(object, level = 0, alpha = 0.05, R = 50, seed = round(r
     }
     LB <- apply(yhat, 1, quantile, probs = alpha / 2)
     UB <- apply(yhat, 1, quantile, probs = 1 - alpha / 2)
-    ans <- data.frame(yhat = predict(object, newdata = newdata, level = level), lower = LB, upper = UB, SE = apply(yhat, 1, sd))
+    ans <- data.frame(yhat = predict(object, newdata = newdata, level = level),
+                      lower = LB,
+                      upper = UB,
+                      SE = apply(yhat, 1, sd))
   } else {
     ans <- list()
     for (j in 1:nq) {
@@ -36,11 +48,14 @@ predint.lqmm <- function(object, level = 0, alpha = 0.05, R = 50, seed = round(r
         tmp$theta_x <- B[i, 1:p, j]
         tmp$theta_z <- B[i, (p + 1):(p + m), j]
         tmp$scale <- B[i, (p + m + 1), j]
-        yhat[, i] <- predict(tmp, newdata = newdata, level = level)
+        yhat[, i] <- predict(tmp, level = level)
       }
       LB <- apply(yhat, 1, quantile, probs = alpha / 2)
       UB <- apply(yhat, 1, quantile, probs = 1 - alpha / 2)
-      ans[[j]] <- data.frame(yhat = predict(tmp, newdata = newdata, level = level), lower = LB, upper = UB, SE = apply(yhat, 1, sd))
+      ans[[j]] <- data.frame(yhat = predict(tmp, newdata = newdata, level = level),
+                             lower = LB,
+                             upper = UB,
+                             SE = apply(yhat, 1, sd))
     }
     names(ans) <- format(tau, digits = 4)
   }
